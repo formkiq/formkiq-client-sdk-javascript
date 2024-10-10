856561d6fbd1b90b7616368736955f28c9b7c914
AnastasiiaPirus <a.pirus2000@gmail.com>
2024-10-10 19:23:44 -0400

    Added sites endpoints

--- build/src/api/SitesApi.js
+++ build/src/api/SitesApi.js
@@ -133,6 +133,82 @@ export class SitesApi {
     const options = this.apiClient.buildOptions('DELETE');
     return await this.apiClient.fetchAndRespond(url, options);
   }
+
+  async addSite({addSiteParameters}) {
+    if (!addSiteParameters) {
+      return JSON.stringify({
+        'message': 'No addSiteParameters specified'
+      });
+    }
+    const url = `${this.apiClient.host}/sites`;
+    const options = this.apiClient.buildOptions('POST', addSiteParameters);
+    return await this.apiClient.fetchAndRespond(url, options);
+  }
+
+  async updateSite({siteId, updateSiteParameters}) {
+    if (!siteId) {
+      return JSON.stringify({
+        'message': 'No siteId specified'
+      });
+    }
+    if (!updateSiteParameters) {
+      return JSON.stringify({
+        'message': 'No updateSiteParameters specified'
+      });
+    }
+    const url = `${this.apiClient.host}/sites/${siteId}`;
+    const options = this.apiClient.buildOptions('PATCH', updateSiteParameters);
+    return await this.apiClient.fetchAndRespond(url, options);
+  }
+
+  async getSiteGroups({siteId}) {
+    if (!siteId) {
+      return JSON.stringify({
+        'message': 'No siteId specified'
+      });
+    }
+    const url = `${this.apiClient.host}/sites/${siteId}/groups`;
+    const options = this.apiClient.buildOptions('GET');
+    return await this.apiClient.fetchAndRespond(url, options);
+  }
+
+  async getSiteGroupPermissions({siteId, groupName}) {
+    if (!siteId) {
+      return JSON.stringify({
+        'message': 'No siteId specified'
+      });
+    }
+    if (!groupName) {
+      return JSON.stringify({
+        'message': 'No groupName specified'
+      });
+    }
+    const url = `${this.apiClient.host}/sites/${siteId}/groups/${groupName}`;
+    const options = this.apiClient.buildOptions('GET');
+    return await this.apiClient.fetchAndRespond(url, options);
+  }
+
+  async setSiteGroupPermissions({siteId, groupName, updateSiteGroupPermissions}) {
+    if (!siteId) {
+      return JSON.stringify({
+        'message': 'No siteId specified'
+      });
+    }
+    if (!groupName) {
+      return JSON.stringify({
+        'message': 'No groupName specified'
+      });
+    }
+    if (!updateSiteGroupPermissions) {
+      return JSON.stringify({
+        'message': 'No updateSiteGroupPermissions specified'
+      });
+    }
+    const url = `${this.apiClient.host}/sites/${siteId}/groups/${groupName}/permissions`;
+    const options = this.apiClient.buildOptions('PUT', updateSiteGroupPermissions);
+    return await this.apiClient.fetchAndRespond(url, options);
+  }
+
 }
 
 export class AddApiKeyParameters {
